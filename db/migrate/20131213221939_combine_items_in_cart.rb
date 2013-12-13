b0f4f45d3686c327061388a80ce02097a91ebb4f
amirtcheva <amirtcheva@gmail.com>
2013-12-13 16:04:01 -0700

    show quantity next to cart items

---
+++ db/migrate/20131213221939_combine_items_in_cart.rb
@@ -0,0 +1,38 @@
+class CombineItemsInCart < ActiveRecord::Migration
+  def change
+  end
+
+  def up
+    #replace multiple items for a single product in a cart with a single item
+    Cart.all.each do |cart|
+      #count the number of each product in the cart
+      sums = cart.line_items.group(:product_id).sum(:quantity)
+      puts "Sums: #{sums}"
+      sums.each do |product_id, quantity|
+        if quantity > 1
+          #remove individual items
+          cart.line_items.where(product_id: product_id).delete_all
+          puts "Product: #{product_id}"
+          puts "Quanity: #{quantity}"
+          #replace with a single item
+          item = cart.line_items.build(product_id: product_id)
+          item.quantity = quantity
+          item.save!
+        end
+      end
+    end
+  end
+
+  def down 
+    #split items with quantity greater than 1 into multiple items
+    LineItem.where("quantity>1").each do |line_item|
+      #add individual items
+      line_item.quantity.times do
+        LineItem.create cart_id: line_item.cart_id, product_id: line_item.product_id, quantity: 1
+      end
+
+      #remove from original item
+      line_item.destroy
+    end
+  end
+end
