16b88fc9d2cbc11cd4ca7948736031942d6edc2e
amirtcheva <amirtcheva@gmail.com>
2013-12-16 12:24:37 -0700

    add order model and view

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,12 +31,12 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to store_url }
-        format.js { @current_item = @line_item }
-        format.json { render action: 'show', status: :created, location: @line_item }
+        format.html { redirect_to store_url; }
+        format.js { @current_item = @line_item; }
+        format.json { render action: 'show', status: :created, location: @line_item; }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render action: 'new'; }
+        format.json { @line_item.errors, status: :unprocessable_entity; render json: }
       end
     end
   end
@@ -46,11 +46,11 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { head :no_content }
+        format.html { @line_item, notice: 'Line item was successfully updated.'; redirect_to }
+        format.json { head :no_content; }
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render action: 'edit'; }
+        format.json { @line_item.errors, status: :unprocessable_entity; render json: }
       end
     end
   end
@@ -60,8 +60,8 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url }
-      format.json { head :no_content }
+      format.html { redirect_to line_items_url; }
+      format.json { head :no_content; }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Store::Application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
