7b962e919a9eb1c38d6abb0cee1c831e2e9ef9b3
amirtcheva <amirtcheva@gmail.com>
2013-12-16 14:28:06 -0700

    mailer for received and shipped orders

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,17 @@
 Store::Application.configure do
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:  "smtp.gmail.com",
+    port:     587,
+    domain:   "gmail.com",
+    authentication: "plain",
+    user_name: "amirtcheva",
+    password:  "hello8899",
+    enable_starttls_auto: true
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
