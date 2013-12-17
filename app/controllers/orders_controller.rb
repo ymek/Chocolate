81aaf9afa1749a7442fcb6ef153e8dbeb36a2673
amirtcheva <amirtcheva@gmail.com>
2013-12-17 07:43:32 -0700

    adding global test helper methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,7 +18,7 @@ class OrdersController < ApplicationController
   # GET /orders/new
   def new
     if @cart.line_items.empty?
-      redirect_to store_ufl, notice: "Your cart is empty"
+      redirect_to store_url, notice: "Your cart is empty"
       return
     end
     @order = Order.new
