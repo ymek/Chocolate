ae7292c64c99dae91e02a92c0a53063ac1c99536
amirtcheva <amirtcheva@gmail.com>
2013-12-16 19:30:30 -0700

    tests for admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Store::Application.routes.draw do
-  get "admin/index"
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
+  get 'admin' => 'admin#index'
+  controller :sessions do
+   get 'login' => :new
+   post 'login' => :create
+   delete 'logout' => :destroy
+  end 
+  
+ # get "sessions/create"
+ # get "sessions/destroy"
   resources :users
-
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   get "store/index"
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,14 +6,21 @@ class SessionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "should login" do
+    alex = users(:one)
+    post :create, name: alex.name, password: 'secret'
+    assert_redirected_to admin_url
+    assert_equal alex.id, session[:user_id]
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
+  test "should fail login" do
+    alex = users(:one)
+    post :create, name: alex.name, password: 'wrong'
+    assert_redirected_to login_url
   end
 
+  test "should logout" do
+    delete :destroy
+    assert_redirected_to store_url
+  end
 end
