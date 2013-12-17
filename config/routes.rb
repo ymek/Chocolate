f17d6b959e5abd6b84604ada15fb83a3c17ec642
amirtcheva <amirtcheva@gmail.com>
2013-12-16 18:43:28 -0700

    adding user model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Store::Application.routes.draw do
+  resources :users
+
   resources :orders
 
   resources :line_items
--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifier.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Awesome Chocolates Order Confirmation", mail.subject
+    assert_equal ["jane@example.com"], mail.to
+    assert_equal ["amirtcheva@gmail.com"], mail.from
+    assert_match /jane doe/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Awesome Chocolates Order Shipped", mail.subject
+    assert_equal ["jane@example.com"], mail.to
+    assert_equal ["amirtcheva@gmail.com"], mail.from
+    #assert_match /<td>&times;<\/td.\s*<td>Chocolate Bar<\/td>/, mail.body.encoded
   end
 
 end
